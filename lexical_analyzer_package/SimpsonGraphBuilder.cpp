f7f05731e2af96f31a7a55b60d2eedc52c9a8d8a
houssainy <mohamed.houssainy@hotmail.com>
2014-04-18 20:09:54 +0200

    Method of SimpsonGraphBuilder done without Test

--- lexical_analyzer_package/SimpsonGraphBuilder.cpp
+++ lexical_analyzer_package/SimpsonGraphBuilder.cpp
@@ -1,7 +1,5 @@
 #include "SimpsonGraphBuilder.h"
 
-#include "../utilities_package/Util.h"
-
 SimpsonGraphBuilder::SimpsonGraphBuilder()
 {
     //ctor
@@ -11,27 +9,95 @@ SimpsonGraphBuilder::SimpsonGraphBuilder()
 *   Build sempson's initial graph for only one input
 **/
 Graph* SimpsonGraphBuilder::init_graph(string value){
-    Graph graph;
+    Graph* graph = new Graph();
     Node * start_node = new Node();
     Node * end_node = new Node();
 
     start_node->add_child(end_node, "\L");
 
-    return &graph;
-}
+    graph->set_start_node(start_node);
+    graph->set_end_node(end_node);
 
-Graph* SimpsonGraphBuilder::positive_clousure_operation(Graph* g1){
-
+    return graph;
 }
-Graph* SimpsonGraphBuilder::clousure_operation(Graph* g1){
-
-}
-Graph* SimpsonGraphBuilder::or_operation(Graph* g1 , Graph* g2){
-
-}
+/**
+*   And operation
+*   e.g: AB
+**/
 Graph* SimpsonGraphBuilder::and_operation(Graph* g1 , Graph* g2){
+        //Connect the two graphs
+        g1->set_end_node(g2->get_start_node());
 
+        return g1;
 }
+
+/**
+*   OR operation
+*   e.g : A|B
+**/
+Graph* SimpsonGraphBuilder::or_operation(Graph* g1 , Graph* g2){
+    Graph* graph = new Graph();
+    Node * start_node = new Node();
+    Node * end_node = new Node();
+
+    // Connect the new End node to the two graphs
+    g1->get_end_node()->add_child(end_node,"\L");
+    g2->get_end_node()->add_child(end_node,"\L");
+
+    //Connect the new Start node to the two graphs
+    start_node->add_child(g1->get_start_node(),"\L");
+    start_node->add_child(g2->get_start_node(),"\L");
+
+    // Add the start and end nodes to the graph
+    graph->set_start_node(start_node);
+    graph->set_end_node(end_node);
+
+    return graph;
+}
+
+/**
+*   Clousure operation
+*   e.g: A*
+**/
+Graph* SimpsonGraphBuilder::clousure_operation(Graph* g1){
+    Graph* graph = new Graph();
+    Node* start_node = new Node();
+    Node* end_node = new Node();
+
+    // 1..*
+    start_node->add_child(g1->get_start_node() , "\L");
+    g1->get_end_node()->add_child(end_node,"\L");
+    g1->get_end_node()->add_child(g1->get_start_node(),"\L");
+
+    //0..*
+    start_node->add_child(end_node , "\L");
+
+    // Add the start and end nodes to the graph
+    graph->set_start_node(start_node);
+    graph->set_end_node(end_node);
+    return graph;
+}
+/**
+*   Clousure operation
+*   e.g: A+
+**/
+Graph* SimpsonGraphBuilder::positive_clousure_operation(Graph* g1){
+    Graph * graph = new Graph();
+    Node * end_node = new Node();
+
+    // 1..+
+    g1->get_end_node()->add_child(g1->get_start_node(),"\L");
+
+    // finish
+    g1->get_end_node()->add_child(end_node,"\L");
+
+    // Add the start and end nodes to the graph
+    graph->set_start_node(g1->get_start_node());
+    graph->set_end_node(end_node);
+
+    return graph;
+}
+
 SimpsonGraphBuilder::~SimpsonGraphBuilder()
 {
     //dtor
