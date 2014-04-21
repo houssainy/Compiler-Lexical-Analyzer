9ea85bff25d8eca283972b289a42d9b55cf15e2f
EnG-MeGo <ahmed.abdelmajied@gmail.com>
2014-04-21 07:20:41 +0200

    Add DFA_Genrator

---
+++ lexical_analyzer_package/DFA_Genrator.cpp
@@ -0,0 +1,11 @@
+#include "DFA_Genrator.h"
+
+DFA_Genrator::DFA_Genrator()
+{
+    //ctor
+}
+
+DFA_Genrator::~DFA_Genrator()
+{
+    //dtor
+}
---
+++ lexical_analyzer_package/DFA_Genrator.h
@@ -0,0 +1,14 @@
+#ifndef DFA_GENRATOR_H
+#define DFA_GENRATOR_H
+
+
+class DFA_Genrator
+{
+    public:
+        DFA_Genrator();
+        virtual ~DFA_Genrator();
+    protected:
+    private:
+};
+
+#endif // DFA_GENRATOR_H
