e4aac184b407a5484bffaba098b5f10ef771c1ca
EnG-MeGo <ahmed.abdelmajied@gmail.com>
2014-04-21 08:01:51 +0200

    modify DFA_Genrator header

--- lexical_analyzer_package/DFA_Genrator.cpp
+++ lexical_analyzer_package/DFA_Genrator.cpp
@@ -1,8 +1,13 @@
 #include "DFA_Genrator.h"
 
-DFA_Genrator::DFA_Genrator()
+
+DFA_Genrator::DFA_Genrator(vector < vector < int > > nfa , vector < vector <int > > eClouser,vector <string> input,vector<bool> finalState)
 {
     //ctor
+
+}
+vector < vector < DFA_State > > DFA_Genrator::Get_DFA (){
+    return DFA;
 }
 
 DFA_Genrator::~DFA_Genrator()
--- lexical_analyzer_package/DFA_Genrator.h
+++ lexical_analyzer_package/DFA_Genrator.h
@@ -1,14 +1,26 @@
 #ifndef DFA_GENRATOR_H
 #define DFA_GENRATOR_H
 
+#include <vector>
+#include <iostream>
+#include <DFA_State.h>
+
+
+using namespace std;
 
 class DFA_Genrator
 {
     public:
-        DFA_Genrator();
+
+        DFA_Genrator(vector < vector < int > > nfa , vector < vector <int > > eClouser,vector <string> input,vector<bool> finalState);
+        vector < vector < DFA_State > > Get_DFA ();
+
         virtual ~DFA_Genrator();
+
     protected:
+
     private:
+        vector < vector < DFA_State > > DFA ;
 };
 
 #endif // DFA_GENRATOR_H
